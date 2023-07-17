2b34487de666ce251f1fcc16f4e4f67509d3ff7d
Syed Osama Ahmed <batch17.026@gmail.com>
2023-07-17 18:41:05 +0500

    learnt primitive data types in kotlin

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -11,15 +11,22 @@ fun main() {
 
     println("Maximum Int Value:$maxIntegerValue, Minimum Int Value:$minIntegerValue")
 
-    val minByteValue=Byte.MIN_VALUE
-    val maxByteValue=Byte.MAX_VALUE
+    val minByteValue = Byte.MIN_VALUE
+    val maxByteValue = Byte.MAX_VALUE
     println("Maximum Byte value:$maxByteValue, Minimum Byte Value:$minByteValue")
 
-    val minShortValue=Short.MIN_VALUE
-    val maxShortValue= Short.MAX_VALUE
+    val minShortValue = Short.MIN_VALUE
+    val maxShortValue = Short.MAX_VALUE
     println("Maximum Short value:$maxShortValue, Minimum Short Value:$minShortValue")
 
-    val minLongValue=Long.MIN_VALUE
-    val maxLongValue=Long.MAX_VALUE
+    val minLongValue = Long.MIN_VALUE
+    val maxLongValue = Long.MAX_VALUE
     println("Maximum Long value:$maxLongValue, Minimum Long Value:$minLongValue")
+
+    val number = 2.5
+    val floatNum = 0.9f
+    val myChar = 'a'
+    val myBool= true
+
+    println("Float: $floatNum, Double: $number , Char: $myChar , Boolean: $myBool ")
 }
