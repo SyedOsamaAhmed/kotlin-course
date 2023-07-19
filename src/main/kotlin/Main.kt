5fad8175daee2ce2b0864eeecea32b38207d58bb
Syed Osama Ahmed <batch17.026@gmail.com>
2023-07-19 16:50:54 +0500

    learnt primitive data types in kotlin

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,32 +1,41 @@
 fun main() {
-    val userName = "Osama"
-    val age = 32
-    println(message = "Hello $userName and my age is $age!")
+    var x = 5
+    val y = 3
+
+
+//    value affected by operator is called operand.
+//    placeholder:
+
     /*
-    Literals: value assigned to the variable
-    val and var: val for readonly variable and var for reassigning variable
-    */
-    val minIntegerValue = Int.MIN_VALUE
-    val maxIntegerValue = Int.MAX_VALUE
+    perform different operations using operators
+     */
+    println(" x + y = ${x + y}")
+    println(" x - y = ${x - y}")
+    println(" x * y = ${x * y}")
+    println(" x / y = ${x / y}")
+    println(" x % y = ${x % y}")
 
-    println("Maximum Int Value:$maxIntegerValue, Minimum Int Value:$minIntegerValue")
 
-    val minByteValue = Byte.MIN_VALUE
-    val maxByteValue = Byte.MAX_VALUE
-    println("Maximum Byte value:$maxByteValue, Minimum Byte Value:$minByteValue")
+    var result = x + y
+    result += 2
+    println(" result = $result")
+    result -= 2
+    println(" result = $result")
+    result *= 2
+    println(" result = $result")
+    result /= 2
+    println(" result = $result")
+    result %= 6
+    println(" result = $result")
 
-    val minShortValue = Short.MIN_VALUE
-    val maxShortValue = Short.MAX_VALUE
-    println("Maximum Short value:$maxShortValue, Minimum Short Value:$minShortValue")
+//learnt operator precedence
+    println(" 3 + 3 * 4 = ${(3 + 3) * 4}")
 
-    val minLongValue = Long.MIN_VALUE
-    val maxLongValue = Long.MAX_VALUE
-    println("Maximum Long value:$maxLongValue, Minimum Long Value:$minLongValue")
+    //Increment and Decrement operators:
+    x = 2
+    println(" x = ${x++}")
+    println(" x = ${++x}")
 
-    val number = 2.5
-    val floatNum = 0.9f
-    val myChar = 'a'
-    val myBool= true
-
-    println("Float: $floatNum, Double: $number , Char: $myChar , Boolean: $myBool ")
+    println(" x = ${x--}")
+    println(" x = ${--x}")
 }
