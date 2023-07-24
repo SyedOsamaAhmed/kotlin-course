c50ecc31f4b896ed19c51232484270567155e93d
Syed Osama Ahmed <batch17.026@gmail.com>
2023-07-24 13:59:43 +0500

    learnt nullable and null with elvis operator

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,23 @@
 fun main() {
-    val alarm=8
+    val alarm = 8
 
 //    when(alarm){
 //        in 1..10-> println("Number is in range 1-10")
 //        12,6,14-> println("The time is $alarm")
 //        else->println("The time is $alarm")
 //    }
-val text=
-    when{
-       !( alarm<=10)->"Number is in range 1-10"
-        alarm==8||alarm==9-> "the time is $alarm"
-        else-> "Time is $alarm"
-    }
-println(text)
+//    val text =
+//        when {
+//            !(alarm <= 10) -> "Number is in range 1-10"
+//            alarm == 8 || alarm == 9 -> "the time is $alarm"
+//            else -> "Time is $alarm"
+//        }
+//    println(text)
+    var name:String?= null
+    println(name?.length)
+    name="the variable is not null"
+    val text2=name?:"The variable is null"
+    println(text2)
+
+
 }
