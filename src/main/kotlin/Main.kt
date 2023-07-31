c8e8306c10abe757da9314158014bdebd6335412
Syed Osama Ahmed <batch17.026@gmail.com>
2023-07-31 13:11:07 +0500

    learnt varargs and for loops and their types in kotlin

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,4 @@
-fun main(args: Array<String>) {
+fun main() {
 //    println(sum(3, 4, 5, 6, 7, 8, 9))
 
     //last no included
@@ -23,22 +23,57 @@ fun main(args: Array<String>) {
 //        println(i)
 //    }
 
-    for(i in 1..10 step 3){
-        println(i)
+//    for(i in 1..10 step 3){
+//        println(i)
+//
+//    }
 
+    //while loop:
+    var number=0
+
+//    while(number<10){
+//        number++
+//       if(number==7)continue
+//        if(number==7)break
+//        println(number)
+//
+//    }
+
+//    for (i in 1..10) {
+//     if(i in 3..7)continue
+//        if (i == 7) break
+//        println(i)
+//    }
+
+    //do while loop:
+//    do {
+//        println(number)
+//    }while (number<10)
+
+
+    //nested loops:
+    outer@ while (number<5){
+        number++
+        println(number)
+
+        var i=0
+        while (i<5){
+            println("*** $i")
+            i++
+        }
     }
 }
 
 //varargs: define as many arguments as you want i.e indefinite numbers
-fun sum(vararg numbers: Int): Int {
-    var result = 0
-    //for in loop
-//    for (number in numbers) {
-//        result += number
+//fun sum(vararg numbers: Int): Int {
+//    var result = 0
+//    //for in loop
+////    for (number in numbers) {
+////        result += number
+////    }
+////for each loop
+//    numbers.forEach {
+//        result += it
 //    }
-//for each loop
-    numbers.forEach {
-        result += it
-    }
-    return result
-}
+//    return result
+//}
